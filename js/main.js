d632693e8830a3e93151730cf4e89a6a01b3ca37
SinaBrauch <sina.brauch@gmail.com>
2022-03-18 11:35:28 +0100

    website adapted for responsive

--- js/main.js
+++ js/main.js
@@ -39,49 +39,6 @@ function toggleMenu() {
 }
 toggle.addEventListener('click', toggleMenu, false);
 
-//image instead of fontawesome as a burger menu
-// function toggleMenu() {
-//   if (menu.classList.contains("active")) {
-//     menu.classList.remove("active");
-//     toggle.querySelector("img").style.width = '100%';
-//     toggle.querySelector("img").src = "img/burger-menu.png";
-//   } else {
-//     menu.classList.add("active");
-//     toggle.querySelector("img").style.width = '0.8em';
-//     toggle.querySelector("img").src = "img/cancel-copy.png";
-//   }
-// }
-
-// Submenu
-const items = document.querySelectorAll('.item');
-
-function toggleItem() {
-  if (this.classList.contains("submenu-active")) {
-    this.classList.remove("submenu-active");
-  } else if (menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
-    this.classList.add("submenu-active");
-  } else {
-    this.classList.add("submenu-active");
-  }
-}
-
-for (let item of items) {
-  if (item.querySelector('.submenu')) {
-    item.addEventListener('click', toggleItem, false);
-    item.addEventListener('keypress', toggleItem, false);
-  }
-}
-
-// Close submenu from anywhere in the page
-function closeSubmenu(e) {
-  let isClickInside = menu.contains(e.target);
-
-  if (!isClickInside && menu.querySelector('.submenu-active')) {
-    menu.querySelector('.submenu-active').classList.remove('submenu-active');
-  }
-}
-document.addEventListener('click', closeSubmenu, false);
 
 // -----------------------------------------------------------------------------
 // TYPEWRITER
